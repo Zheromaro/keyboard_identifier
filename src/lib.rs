99bb622f43b71fc28a2e006019a886082edcb68c
Dib Zahradin <zahradin.dib@gmail.com>
2026-08-02 10:21:32 +0100

    start planing

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,19 @@
-use std::collections::HashMap;
+type KeyboardID = String;
 
-// A simple platform-agnostic key event
-#[derive(Debug, PartialEq, Eq, Clone)]
-pub struct KeyboardEvent {
-    pub device_id: String,
-    pub key_code: u16,
+fn keyboard_listener() {
+    // TODO: make an infinite loop
+    // TODO: upadte keyboards list (check if a keyboard in unpluged or pluged)
+    // TODO: if a key is presed: indentify the keyboard, get the char, and call on_key_pressed(id: KeyboardID, c: char)
 }
 
-// State manager tracking connected keyboards or history
-#[derive(Default)]
-pub struct KeyboardTracker {
-    // Maps Device ID -> List of keycodes pressed
-    pub history: HashMap<String, Vec<u16>>,
+pub fn init_keyboard_listener() {
+    // TODO: start the keyboard listener thread
 }
 
-impl KeyboardTracker {
-    pub fn new() -> Self {
-        Self::default()
-    }
+pub fn end_keyboard_listener() {
+    // TODO: stop the keyboard listener thread
+}
 
-    pub fn process_event(&mut self, event: KeyboardEvent) {
-        self.history
-            .entry(event.device_id)
-            .or_default()
-            .push(event.key_code);
-    }
+pub fn on_key_pressed(id: KeyboardID, c: char) -> (KeyboardID, char) {
+    (id, c)
 }
